e5bb570800db9f36afa09d391734d5c3cb41ac3d
TalhaShaikh150 <tssmart233@gmail.com>
2025-05-07 01:38:22 +0500

    Local Storage added

--- app.js
+++ app.js
@@ -3,47 +3,73 @@ const firstScreen = document.querySelector(".first-screen");
 
 const startBtn = document.querySelector(".js-start-btn");
 
-const todoContainer = document.querySelector(".todo-container");
+let todoContainer = document.querySelector(".todo-container");
 
-const addBtnContainer = document.querySelector(".add-btn-container");
+const todoBtn = document.querySelector(".todo-button");
+
+let inputBox = document.querySelector(".pop-up");
+let mainContainer = document.querySelector("main");
+let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
 
 startBtn.addEventListener("click", () => {
   firstScreen.classList.add("none");
+  todoBtn.classList.remove("none");
   renderDate();
   renderTodo();
-  generateButton();
+  renderInputPage();
 });
 
 function renderTodo() {
-  todoContainer.innerHTML += `<h1 class="today">Today</h1>
-  `;
-  todoContainer.innerHTML += `
-      <span class="category">Personal</span>
+  document.querySelector(".today").classList.remove("none");
+  let html = "";
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    const { category, name, dueDate } = todoObject;
+    html += `
+    <div class="todoBox">
+    <span class="category">${category}</span>
     <div class="todo">
-    <input type="checkbox" />
-    <p>Exercise For 30 Mintues</p>
+    <div class="align">
+    <input type="checkbox" class="checkbox"/>
+    <label>${name}</label>
+    </div>      
+    <p>${dueDate}</p>
+    </div>
     </div>
     `;
+  }
+  todoContainer.innerHTML = html;
+  inputBox.classList.remove("visible");
+  mainContainer.classList.remove("none");
 }
 
-function generateButton() {
-  addBtnContainer.innerHTML += `<button class="todo-btn add-todo-btn">
-    <i class="fa-solid fa-plus"></i>
-    </button>`;
-
-  document.querySelector(".add-todo-btn").addEventListener("click", () => {
-    let mainContainer = document.querySelector("main");
+function renderInputPage() {
+  todoBtn.addEventListener("click", () => {
     mainContainer.classList.add("none");
-    document.body.innerHTML += `<div class="pop-up">
-      <input type="text" placeholder="Enter Your Task" class="js-todo" />
-      <input type="date" class="js-date" />
-      <input type="text" placeholder="Category" class="js-category" />
-    </div>`;
+    todoBtn.classList.add("none");
+    inputBox.classList.add("visible");
   });
 }
 
-function showInput() {
-  const todoInput = document.querySelector(".js-todo");
-  const dateInput = document.querySelector(".js-date");
-  const categoryInput = document.querySelector(".js-category");
+function addTodo() {
+  const todoElement = document.querySelector(".js-todo");
+  const dueDateElement = document.querySelector(".js-date");
+  const categoryElement = document.querySelector(".js-category");
+  const name = todoElement.value;
+  const dueDate = dueDateElement.value;
+  const category = categoryElement.value;
+  todoList.push({ name, dueDate, category });
+  saveToStorage();
+  todoBtn.classList.remove("none");
+  renderTodo();
+
+  todoElement.value = "";
+  dueDateElement.value = "";
+  categoryElement.value = "";
+}
+
+document.querySelector(".add-todo").addEventListener("click", addTodo);
+
+function saveToStorage() {
+  localStorage.setItem("todoList", JSON.stringify(todoList));
 }
