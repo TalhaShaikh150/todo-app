49c3bf152d1701f13cf0aa0fa2c67d43d102c923
TalhaShaikh150 <tssmart233@gmail.com>
2025-05-07 20:14:04 +0500

    Created Seperate Folders

--- scripts/app.js
+++ scripts/app.js
@@ -9,6 +9,7 @@ const todoBtn = document.querySelector(".todo-button");
 
 let inputBox = document.querySelector(".pop-up");
 let mainContainer = document.querySelector("main");
+
 let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
 
 startBtn.addEventListener("click", () => {
@@ -62,14 +63,19 @@ function addTodo() {
   saveToStorage();
   todoBtn.classList.remove("none");
   renderTodo();
+  let btnContainer = document.querySelector(".btn-container");
+  let categoryBtn = `<button class="category-btn">${categoryElement.value}</button>`;
 
   todoElement.value = "";
   dueDateElement.value = "";
   categoryElement.value = "";
+
+  btnContainer.innerHTML += categoryBtn;
 }
 
 document.querySelector(".add-todo").addEventListener("click", addTodo);
 
+//Local Storage
 function saveToStorage() {
   localStorage.setItem("todoList", JSON.stringify(todoList));
 }
