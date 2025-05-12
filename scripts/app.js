4c806c1b63194330723e47d491c70dc36fbbd732
TalhaShaikh150 <tssmart233@gmail.com>
2025-05-12 14:11:11 +0500

    Added Categories

--- scripts/app.js
+++ scripts/app.js
@@ -5,8 +5,10 @@ const startBtn = document.querySelector(".js-start-btn");
 
 let todoContainer = document.querySelector(".todo-container");
 
+const btnContainer = document.querySelector(".btn-container");
 const todoBtn = document.querySelector(".todo-button");
 
+const emptyMessage = document.querySelector(".empty-container");
 let inputBox = document.querySelector(".pop-up");
 let mainContainer = document.querySelector("main");
 
@@ -21,7 +23,9 @@ startBtn.addEventListener("click", () => {
 });
 
 function renderTodo() {
+  todoBtn.classList.remove("none");
   document.querySelector(".today").classList.remove("none");
+  document.querySelector(".category-btn").classList.remove("none");
   let html = "";
   for (let i = 0; i < todoList.length; i++) {
     const todoObject = todoList[i];
@@ -40,6 +44,11 @@ function renderTodo() {
     `;
   }
   todoContainer.innerHTML = html;
+
+  if (todoContainer.innerHTML === "") {
+    emptyMessage.classList.remove("none");
+  }
+
   inputBox.classList.remove("visible");
   mainContainer.classList.remove("none");
 }
@@ -49,6 +58,7 @@ function renderInputPage() {
     mainContainer.classList.add("none");
     todoBtn.classList.add("none");
     inputBox.classList.add("visible");
+    emptyMessage.classList.add("none");
   });
 }
 
@@ -60,22 +70,66 @@ function addTodo() {
   const dueDate = dueDateElement.value;
   const category = categoryElement.value;
   todoList.push({ name, dueDate, category });
-  saveToStorage();
   todoBtn.classList.remove("none");
+
   renderTodo();
-  let btnContainer = document.querySelector(".btn-container");
-  let categoryBtn = `<button class="category-btn">${categoryElement.value}</button>`;
+  let categoryBtn = `<button class="category-btn js-category-btn">${categoryElement.value}</button>`;
 
   todoElement.value = "";
   dueDateElement.value = "";
   categoryElement.value = "";
 
   btnContainer.innerHTML += categoryBtn;
+  saveToStorage();
+
+  let newCategoryBtn = document.querySelectorAll(".category-btn");
+  newCategoryBtn.forEach((newBtn) => {
+    newBtn.addEventListener("click", () => {
+      toggler(newBtn);
+      showCategory(newBtn);
+    });
+  });
 }
 
 document.querySelector(".add-todo").addEventListener("click", addTodo);
 
-//Local Storage
+document.querySelector(".back-todo").addEventListener("click", renderTodo);
+
 function saveToStorage() {
   localStorage.setItem("todoList", JSON.stringify(todoList));
+}
+
+function toggler(newBtn) {
+  if (!newBtn.classList.contains("is-toggled")) {
+    turnOffPeviousButton();
+    newBtn.classList.add("is-toggled");
+  } else {
+    newBtn.classList.remove("is-toggled");
+  }
+  function turnOffPeviousButton() {
+    const previousButton = document.querySelector(".is-toggled");
+    if (previousButton) {
+      previousButton.classList.remove("is-toggled");
+    }
+  }
+}
+
+function showCategory(newBtn) {
+  let updatedHtml = "";
+  for (let i = 0; i < todoList.length; i++) {
+    if (newBtn.innerHTML === todoList[i].category) {
+      // console.log(todoList[i].name);
+      updatedHtml += ` <div class="todoBox">
+    <span class="category">${todoList[i].category}</span>
+    <div class="todo">
+    <div class="align">
+    <input type="checkbox" class="checkbox"/>
+    <label>${todoList[i].name}</label>
+    </div>      
+    <p>${todoList[i].dueDate}</p>
+    </div>
+    </div>`;
+      todoContainer.innerHTML = updatedHtml;
+    }
+  }
 }
