795c2023d9b53cd3a7b6670cf6a4a15503b2c3be
TalhaShaikh150 <tssmart233@gmail.com>
2025-05-13 21:52:46 +0500

    Local Storage Added On Buttons And Fixed Some Bugs

--- scripts/app.js
+++ scripts/app.js
@@ -14,9 +14,22 @@ let mainContainer = document.querySelector("main");
 
 let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
 
+let localBtn = JSON.parse(localStorage.getItem("button")) || [];
+
 startBtn.addEventListener("click", () => {
   firstScreen.classList.add("none");
   todoBtn.classList.remove("none");
+  localBtn.forEach((btnHTML) => {
+    btnContainer.innerHTML += btnHTML;
+  });
+  let newCategoryBtn = document.querySelectorAll(".category-btn");
+
+  newCategoryBtn.forEach((newBtn) => {
+    newBtn.addEventListener("click", () => {
+      toggleBtn();
+      showCategory(newBtn);
+    });
+  });
   renderDate();
   renderTodo();
   renderInputPage();
@@ -73,53 +86,48 @@ function addTodo() {
   todoBtn.classList.remove("none");
 
   renderTodo();
-  let categoryBtn = `<button class="category-btn js-category-btn">${categoryElement.value}</button>`;
+  let categoryBtn = `<button class="category-btn js-category-btn">${category}</button>`;
 
   todoElement.value = "";
   dueDateElement.value = "";
   categoryElement.value = "";
 
-  btnContainer.innerHTML += categoryBtn;
-  saveToStorage();
+  if (!localBtn.includes(categoryBtn)) {
+    localBtn.push(categoryBtn);
+    localStorage.setItem("button", JSON.stringify(localBtn));
+    btnContainer.innerHTML += categoryBtn;
+    let newCategoryBtn = document.querySelectorAll(".category-btn");
 
-  let newCategoryBtn = document.querySelectorAll(".category-btn");
-  newCategoryBtn.forEach((newBtn) => {
-    newBtn.addEventListener("click", () => {
-      toggler(newBtn);
-      showCategory(newBtn);
+    newCategoryBtn.forEach((newBtn) => {
+      newBtn.addEventListener("click", () => {
+        toggleBtn();
+        showCategory(newBtn);
+      });
     });
-  });
+  }
+  saveToStorage();
 }
 
 document.querySelector(".add-todo").addEventListener("click", addTodo);
 
 document.querySelector(".back-todo").addEventListener("click", renderTodo);
-
-function saveToStorage() {
-  localStorage.setItem("todoList", JSON.stringify(todoList));
-}
-
-function toggler(newBtn) {
-  if (!newBtn.classList.contains("is-toggled")) {
-    turnOffPeviousButton();
-    newBtn.classList.add("is-toggled");
-  } else {
-    newBtn.classList.remove("is-toggled");
-  }
-  function turnOffPeviousButton() {
-    const previousButton = document.querySelector(".is-toggled");
-    if (previousButton) {
-      previousButton.classList.remove("is-toggled");
-    }
-  }
+function toggleBtn() {
+  const buttons = document.querySelectorAll(".category-btn");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      buttons.forEach((btn) => btn.classList.remove("is-toggled"));
+      button.classList.add("is-toggled");
+    });
+  });
 }
 
 function showCategory(newBtn) {
   let updatedHtml = "";
   for (let i = 0; i < todoList.length; i++) {
-    if (newBtn.innerHTML === todoList[i].category) {
-      // console.log(todoList[i].name);
-      updatedHtml += ` <div class="todoBox">
+    if (newBtn.innerHTML === "All") {
+      renderTodo();
+    } else if (newBtn.innerHTML === todoList[i].category) {
+      updatedHtml += `<div class="todoBox">
     <span class="category">${todoList[i].category}</span>
     <div class="todo">
     <div class="align">
@@ -132,4 +140,8 @@ function showCategory(newBtn) {
       todoContainer.innerHTML = updatedHtml;
     }
   }
+}
+
+function saveToStorage() {
+  localStorage.setItem("todoList", JSON.stringify(todoList));
 }
